efd610f8adfcf4e130dd63c9ec70371c08651989
ssunny <ssssunny85@gmail.com>
2021-05-10 15:02:28 +0900

    [WIP] google 로그인 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
 import Login from './components/login';
 import './App.css';
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: `${process.env.REACT_APP_API_KEY}.firebaseapp.com`,
-  projectId: process.env.REACT_APP_API_KEY,
-  storageBucket: `${process.env.REACT_APP_API_KEY}.appspot.com`,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-firebase.initializeApp(firebaseConfig);
 
 const App = () => (
   <div className="app">
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import './style.css';
 
-const Login = () => (
-  <section className="login">
-    <h1>Card Maker</h1>
-    <div>
-      <button type="button">Google</button>
-      <button type="button">Github</button>
-    </div>
-  </section>
-);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
+const Login = () => {
+  const handleLoginGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const { credential, user } = result;
+        const { accessToken } = credential;
+        console.log('accessToken: ', accessToken);
+        console.log('user: ', user);
+      })
+      .catch((error) => {
+        console.log('error: ', error);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // const email = error.email;
+        // const credential = error.credential;
+      });
+  };
+  return (
+    <section className="login">
+      <h1>Card Maker</h1>
+      <div>
+        <button type="button" onClick={handleLoginGoogle}>
+          Google LogIn
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
